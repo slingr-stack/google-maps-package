3cd2e1eaa79aca31c26c2c3083654fa0acaf735f
Alejandro Greggio <agreggio@slingr.io>
2024-02-09 14:43:04 -0300

    #15800 - Task : Create Google Maps Package

--- scripts/api.js
+++ scripts/api.js
@@ -261,7 +261,7 @@ function setApiUri(options) {
 
 function setRequestHeaders(options) {
     let headers = options.headers || {};
-    headers = mergeJSON(headers, {"Authorization": "key" + config.get("apiKey")});
+    headers = mergeJSON(headers, {"key" : config.get("apiKey")});
     headers = mergeJSON(headers, {"Content-Type": "application/json"});
     options.headers = headers;
     return options;
